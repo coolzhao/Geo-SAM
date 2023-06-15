5a62453758d9800f2bcbf38b8724624c2124123d
fanchengyan <fanchy14@lzu.edu.cn>
2023-06-15 13:05:22 +0800

    auto load UI file as class

--- ui/UI.py
+++ ui/UI.py
@@ -1,6 +1,13 @@
+import os
 from PyQt5 import uic
+try:
+    from qgis.PyQt.QtGui import QDockWidget, QWidget
+except:
+    from qgis.PyQt.QtWidgets import QDockWidget, QWidget
 
-UI_Selector = uic.loadUi("Selector.ui")
-UI_Encoder = uic.loadUi("Encoder.ui")
+uiFileDir = os.path.abspath(os.path.dirname(__file__))
+selector_path = os.path.join(uiFileDir, "Selector.ui")
+encoder_path = os.path.join(uiFileDir, "Encoder.ui")
 
-
+UI_Selector = uic.loadUi(selector_path)
+UI_Encoder = uic.loadUi(encoder_path)
