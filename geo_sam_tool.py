e9d7c8ed5df312317334dffe55f2fdf2fc095a23
fanchengyan <fanchy14@lzu.edu.cn>
2023-06-20 02:11:39 +0800

    add shortcut for buttons & make it useable ot topping lay func

--- geo_sam_tool.py
+++ geo_sam_tool.py
@@ -1,6 +1,6 @@
 import os
-import typing
-from qgis.core import QgsProject, Qgis, QgsMessageLog
+from typing import List
+from qgis.core import QgsProject, Qgis, QgsMessageLog, QgsApplication
 from qgis.gui import QgsMapToolPan
 from qgis.core import QgsRasterLayer
 from qgis.PyQt.QtWidgets import QDockWidget
@@ -9,6 +9,9 @@ from PyQt5.QtWidgets import (
     QFileDialog,
     QAction,
     QFileDialog,
+    QApplication,
+    QShortcut
+
 )
 from PyQt5.QtGui import QKeySequence, QIcon, QColor
 from PyQt5 import uic
@@ -21,7 +24,6 @@ from .ui import UI_Selector
 
 class Geo_SAM(QObject):
     execute_SAM = pyqtSignal()
-    activate_fg = pyqtSignal()
 
     def __init__(self, iface, cwd: str):
         super().__init__()
@@ -33,6 +35,8 @@ class Geo_SAM(QObject):
         # feature_dir = r"D:\Data\sam_data\features\mosaic_scripts_wp_4.77_sub_114"
         self.feature_dir = feature_dir
         self.toolPan = QgsMapToolPan(self.canvas)
+        self.dockFirstOpen = True
+        self.prompts: List = []
 
     def initGui(self):
         icon_path = os.path.join(self.cwd, "icons/geo_sam_tool.svg")
@@ -69,18 +73,28 @@ class Geo_SAM(QObject):
     def unload(self):
         self.iface.removeToolBarIcon(self.action)
         self.iface.removePluginMenu('&Geo-SAM', self.action)
+        self._clear_layers()
+        
+        # TODO: could use stortcut after reload plugin
+        if hasattr(self, "shortcut_undo"):
+            self.shortcut_undo.disconnect()
+        if hasattr(self, "shortcut_save"):
+            self.shortcut_save.disconnect()
+        if hasattr(self, "shortcut_clear"):
+            self.shortcut_clear.disconnect()
+            
         del self.action
 
     def topping_polygon_sam_layer(self):
         if hasattr(self, "polygon"):
-            rg = self.iface.layerTreeCanvasBridge().rootGroup()
-            order = rg.layerOrder()
-            if self.polygon.layer in order and order[0] != self.polygon.layer:
-                order.remove(self.polygon.layer)
-                order.insert(0, self.polygon.layer)
-            rg.setCustomLayerOrder(order)
+            root = QgsProject.instance().layerTreeRoot()
+            tree_layer = root.findLayer(self.polygon.layer.id())
+            tl_clone = tree_layer.clone()
+            root.insertChildNode(0, tl_clone)
+            root.removeChildNode(tree_layer)
 
     def clear_canvas_layers_safely(self):
+        '''Clear canvas layers safely'''
         if hasattr(self, "canvas_points"):
             self.canvas_points.clear()
         if hasattr(self, "canvas_rect"):
@@ -89,8 +103,8 @@ class Geo_SAM(QObject):
     def _init_feature_related(self):
         '''Init or reload feature related objects'''
 
-        # clear canvas layers when loading new image
-        self.clear_canvas_layers_safely()
+        # clear layers when loading new image
+        # self.clear_layers()
 
         # init feature related objects
         self.sam_model = SAM_Model(self.feature_dir, self.cwd)
@@ -105,78 +119,121 @@ class Geo_SAM(QObject):
         canvas.setExtent(extent_canvas)
         canvas.refresh()
 
-        # self.shortcut_undo = QShortcut(QKeySequence(Qt.ControlModifier + Qt.Key_Z), self.iface.mainWindow())
-        # self.shortcut_undo = QShortcut(QKeySequence("Ctrl+Z"), self.iface.mainWindow())
-        # self.shortcut_undo.setContext(Qt.ApplicationShortcut)
-        # self.shortcut_undo.activated.connect(self.execute_segmentation)
-
         # init tools
         self.tool_click_fg = ClickTool(
             self.canvas,
             self.canvas_points,
             'fgpt',
+            self.prompts,
             self.execute_SAM,
         )
         self.tool_click_bg = ClickTool(
             self.canvas,
             self.canvas_points,
             'bgpt',
+            self.prompts,
             self.execute_SAM,
         )
         self.tool_click_rect = RectangleMapTool(
-            self.canvas_rect, self.execute_SAM, self.img_crs_manager
+            self.canvas_rect, self.prompts, self.execute_SAM, self.img_crs_manager
         )
 
     def create_widget_selector(self):
         '''Create widget selector'''
+        QgsMessageLog.logMessage(
+            f'create widget selector: dockFirstOpen : {self.dockFirstOpen} ', 'Geo SAM', Qgis.Info)
+        if self.dockFirstOpen:
+            self._init_feature_related()
+            self.load_demo_img()
+            receiversCount = self.receivers(self.execute_SAM)
+            if receiversCount == 0:
+                self.execute_SAM.connect(self.execute_segmentation)
 
-        # clear canvas layers if reloaded
-        self.clear_canvas_layers_safely()
+            self.wdg_sel = UI_Selector
+            self.wdg_sel.pushButton_fg.clicked.connect(
+                self.draw_foreground_point)
+            self.wdg_sel.pushButton_bg.clicked.connect(
+                self.draw_background_point)
+            self.wdg_sel.pushButton_rect.clicked.connect(self.draw_rect)
+            self.wdg_sel.pushButton_clear.clicked.connect(self.clear_layers)
+            self.wdg_sel.pushButton_find_file.clicked.connect(self.find_file)
+            self.wdg_sel.pushButton_load_file.clicked.connect(
+                self.load_shp_file)
+            self.wdg_sel.pushButton_save.clicked.connect(self.save_shp_file)
+            self.wdg_sel.pushButton_find_feature.clicked.connect(
+                self.find_feature)
+            self.wdg_sel.pushButton_load_feature.clicked.connect(
+                self.load_feature)
 
-        self._init_feature_related()
-        self.load_demo_img()
-        receiversCount = self.receivers(self.execute_SAM)
-        if receiversCount == 0:
-            self.execute_SAM.connect(self.execute_segmentation)
-            self.activate_fg.connect(self.draw_foreground_point)
+            self.wdg_sel.radioButton_enable.setChecked(True)
+            self.wdg_sel.radioButton_enable.toggled.connect(
+                self.enable_disable)
+            self.wdg_sel.pushButton_fg.setCheckable(True)
+            self.wdg_sel.pushButton_bg.setCheckable(True)
+            self.wdg_sel.pushButton_rect.setCheckable(True)
 
-        self.wdg_sel = UI_Selector
-        self.wdg_sel.pushButton_fg.clicked.connect(self.draw_foreground_point)
-        self.wdg_sel.pushButton_bg.clicked.connect(self.draw_background_point)
-        self.wdg_sel.pushButton_rect.clicked.connect(self.draw_rect)
-        self.wdg_sel.pushButton_clear.clicked.connect(self.clear_layers)
-        self.wdg_sel.pushButton_find_file.clicked.connect(self.find_file)
-        self.wdg_sel.pushButton_load_file.clicked.connect(self.load_shp_file)
-        self.wdg_sel.pushButton_save.clicked.connect(self.save_shp_file)
-        self.wdg_sel.pushButton_find_feature.clicked.connect(self.find_feature)
-        self.wdg_sel.pushButton_load_feature.clicked.connect(self.load_feature)
+            self.wdg_sel.setFloating(True)
 
-        self.wdg_sel.radioButton_enable.setChecked(True)
-        self.wdg_sel.radioButton_enable.toggled.connect(self.enable_disable)
-        self.wdg_sel.pushButton_fg.setCheckable(True)
-        self.wdg_sel.pushButton_bg.setCheckable(True)
-        self.wdg_sel.pushButton_rect.setCheckable(True)
-        # self.wdg_sel.closed.connect(self.destruct()) # qgis.gui.QgsDockWidget, actually QDockWidget no closed signal
-        # self.wdg_sel.closeEvent = self.destruct
-        # self.wdg_sel.visibilityChanged.connect(self.destruct)
+            # qgis.gui.QgsDockWidget, actually QDockWidget no closed signal
+            self.wdg_sel.closed.connect(self.destruct)
+            # self.wdg_sel.closeEvent = self.destruct
+            # self.wdg_sel.visibilityChanged.connect(self.destruct)
+
+            self.shortcut_save = QShortcut(
+                QKeySequence("S"), self.iface.mainWindow())
+            self.shortcut_save.activated.connect(self.save_shp_file)
+
+            self.shortcut_undo = QShortcut(
+                QKeySequence('Z'), self.iface.mainWindow())
+            self.shortcut_undo.activated.connect(self.undo_last_prompt)
+
+            self.shortcut_clear = QShortcut(
+                QKeySequence('C'), self.iface.mainWindow())
+            self.shortcut_clear.activated.connect(self.clear_layers)
+
+            self.dockFirstOpen = False
+        else:
+            self.clear_layers()
+        
+        
 
         # add widget to QGIS
-        self.wdg_sel.setFloating(True)
         self.iface.addDockWidget(Qt.TopDockWidgetArea, self.wdg_sel)
 
         # default is fgpt, but do not change when reloading feature folder
         self.reset_prompt_type()
+        
+    def register_shortcuts(self):
+        # Unregister existing shortcuts, if any
+        self.iface.unregisterMainWindowActions()
+        
+        # Register new shortcuts
+        action = self.iface.registerMainWindowAction("my_plugin:my_action", "My Action", self.myAction, QKeySequence("Ctrl+M"))
+        self.iface.addPluginToMenu("&My Plugin", action)
+
+    def undo_last_prompt(self):
+        if len(self.prompts) > 0:
+            prompt_last = self.prompts.pop()
+            QgsMessageLog.logMessage(
+                f'undo last prompt {prompt_last}', 'Geo SAM', Qgis.Info)
+            if prompt_last == 'bbox':
+                self.canvas_rect.clear()
+            else:
+                if len(self.canvas_points.markers) > 0:
+                    self.canvas_points.popPoint()
+            self.execute_SAM.emit()
 
     def destruct(self):
-        self.save_shp_file()
-        receiversCount = self.receivers(self.execute_SAM)
-        if receiversCount > 0:
-            self.execute_SAM.disconnect()
-            self.activate_fg.disconnect()
+        '''Destruct actions when closed widget'''
+        # TODO: make it work
+        self.iface.mapCanvas().unsetMapTool(self.tool_click_bg)
+        self.iface.mapCanvas().unsetMapTool(self.tool_click_fg)
+        self.iface.mapCanvas().unsetMapTool(self.tool_click_rect)
         self.canvas.setMapTool(self.toolPan)
-        # self.wdg_sel.destroy()
+        self.clear_layers()
 
     def enable_disable(self):
+        '''Enable or disable the widget selector'''
         radioButton = self.sender()
         if not radioButton.isChecked():
             # UI_Selector.setEnabled(False)
@@ -216,6 +273,7 @@ class Geo_SAM(QObject):
         self.topping_polygon_sam_layer()
 
     def draw_foreground_point(self):
+        '''draw foreground point in canvas'''
         self.canvas.setMapTool(self.tool_click_fg)
         button = self.wdg_sel.pushButton_fg  # self.sender()
         if button.isChecked():
@@ -231,6 +289,7 @@ class Geo_SAM(QObject):
         self.prompt_type = 'fgpt'
 
     def draw_background_point(self):
+        '''draw background point in canvas'''
         self.canvas.setMapTool(self.tool_click_bg)
         button = self.wdg_sel.pushButton_bg  # self.sender()
         if button.isChecked():
@@ -246,6 +305,7 @@ class Geo_SAM(QObject):
         self.prompt_type = 'bgpt'
 
     def draw_rect(self):
+        '''draw rectangle in canvas'''
         self.canvas.setMapTool(self.tool_click_rect)
         button = self.wdg_sel.pushButton_rect  # self.sender()
         if button.isChecked():
@@ -262,19 +322,23 @@ class Geo_SAM(QObject):
         self.prompt_type = 'bbox'
 
     def find_file(self):
+        '''find shapefile path'''
         path, _ = QFileDialog.getSaveFileName(None, "Save shapefile", "")
         self.wdg_sel.path_out.setText(path)
 
     def load_shp_file(self):
+        '''load shapefile'''
         text = self.wdg_sel.path_out.text()
         self.polygon = SAM_PolygonFeature(self.img_crs_manager, text)
 
     def find_feature(self):
+        '''find feature directory'''
         feature_dir_str = QFileDialog.getExistingDirectory(
             None, "get feature directory", "")
         self.wdg_sel.path_feature.setText(feature_dir_str)
 
     def load_feature(self):
+        '''load feature'''
         self.feature_dir = self.wdg_sel.path_feature.text()
         if self.feature_dir is not None and os.path.exists(self.feature_dir):
             self.clear_layers()
@@ -286,20 +350,27 @@ class Geo_SAM(QObject):
             self.iface.messageBar().pushMessage("Feature folder not exist",
                                                 "choose a another folder", level=Qgis.Info)
 
-    def clear_layers(self):
+    def _clear_layers(self):
+        '''Clear all temporary layers (canvas and new sam result)'''
         self.clear_canvas_layers_safely()
         if hasattr(self, "polygon"):
             self.polygon.rollback_changes()
-        # self.activate_fg.emit()
+
+    def clear_layers(self):
+        '''Clear all temporary layers (canvas and new sam result) and reset prompt type'''
+        self.clear_canvas_layers_safely()
+        if hasattr(self, "polygon"):
+            self.polygon.rollback_changes()
         self.reset_prompt_type()
 
     def save_shp_file(self):
+        '''save sam result into shapefile layer'''
         if hasattr(self, "polygon"):
             self.polygon.commit_changes()
-        self.clear_layers()
-        # self.activate_fg.emit()
+        self.clear_canvas_layers_safely()
 
     def reset_prompt_type(self):
+        '''reset prompt type'''
         if hasattr(self, "prompt_type"):
             if self.prompt_type == 'bbox':
                 self.draw_rect()
