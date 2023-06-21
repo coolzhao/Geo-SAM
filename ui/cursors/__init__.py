87286e8693449840b62f562fe23ce04ff37ecc3c
fanchengyan <fanchy14@lzu.edu.cn>
2023-06-21 10:22:18 +0800

    add cursors for tools & restructure the ui dir

---
+++ ui/cursors/__init__.py
@@ -0,0 +1,26 @@
+import os
+from qgis.core import Qgis,QgsApplication
+from PyQt5.QtGui import QIcon, QCursor
+from PyQt5.QtCore import QSize
+
+__all__ = ['CursorPointBlue', 'CursorPointRed', 'CursorRect']
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+CursorPointBlue_path = os.path.join(cwd, "CursorPointBlue.svg")
+CursorPointRed_path = os.path.join(cwd, "CursorPointRed.svg")
+CursorRect_path = os.path.join(cwd, "CursorRect.svg")
+
+# scaling ref: https://github.com/qgis/QGIS/blob/11c77af3dd95fb1f5bb4ce3a4ef5dc97de951ec5/src/core/qgsapplication.cpp#L873
+scale = Qgis.UI_SCALE_FACTOR * QgsApplication.fontMetrics().height() / 32.0
+# QIcon("filepath.svg").pixmap(QSize()) https://stackoverflow.com/a/36936216
+
+
+CursorPointBlueBitmap = QIcon(CursorPointBlue_path).pixmap(QSize(scale*32, scale*32))
+CursorPointBlue = QCursor(CursorPointBlueBitmap)
+
+CursorPointRedBitmap = QIcon(CursorPointRed_path).pixmap(QSize(scale*32, scale*32))
+CursorPointRed = QCursor(CursorPointRedBitmap)
+
+CursorRectBitmap = QIcon(CursorRect_path).pixmap(QSize(scale*32, scale*32))
+CursorRect = QCursor(CursorRectBitmap)
---
+++ ui/icons/__init__.py
@@ -0,0 +1,9 @@
+import os
+from PyQt5.QtGui import QIcon
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+geo_sam_tool_path = os.path.join(cwd, 'geo_sam_tool.svg')
+encoder_tool_path = os.path.join(cwd, 'encoder_tool.svg')
+
+QIcon_GeoSAMTool = QIcon(geo_sam_tool_path)
+QIcon_EncoderTool = QIcon(encoder_tool_path)
